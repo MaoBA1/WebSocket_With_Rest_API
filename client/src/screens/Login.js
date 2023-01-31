61b705d37204bc944b07dd63ab9bc2f7ab898c36
Maor Bar Asher <94516860+MaoBA1@users.noreply.github.com>
2023-01-31 14:00:33 +0200

    123

--- client/src/screens/Login.js
+++ client/src/screens/Login.js
@@ -1,5 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import Colors from '../utilities/Colors';
+
+const Header = (windowSize) => {
+    return(
+        <div style={{
+            backgroundColor:Colors.blackBlue,
+            width: windowSize.width,
+            height: windowSize.height / 10,
+            display:"flex",
+            flexDirection:"row",
+            alignItems:"center",
+            justifyContent:"center"
+        }}>
+            <div style={{
+                display:"flex",
+                flexDirection:"column", 
+                alignItems:"center"
+            }}>
+                <label style={{
+                    color:"#FFFFFF",
+                    fontSize:"30px",
+                    fontFamily:"Bold",
+                }}>
+                    Posts & Chats
+                </label>
+                <label style={{
+                    color:"#FFFFFF",
+                    fontSize:"25px",
+                    fontFamily:"Regular",
+                    fontStyle:"italic"
+                }}>
+                    Come talk with us
+                </label>
+            </div>
+        </div>
+    )
+}
+
+
+
 function Login(props) {
 
     const [ windowSize, setWindowSize ] = useState({
@@ -28,37 +67,38 @@ function Login(props) {
             height: windowSize.height,
             backgroundColor: Colors.creamyWhite
         }}>
-            <div style={{
-                backgroundColor:Colors.blackBlue,
-                width: windowSize.width,
-                height: windowSize.height / 10,
+           <Header windowSize={windowSize}/>
+           <div style={{
+                flex:"1",
                 display:"flex",
-                flexDirection:"row",
+                flexDirection:"column",
+                width:windowSize.width,
+                height: windowSize.height - (windowSize.height / 10),
                 alignItems:"center",
                 justifyContent:"center"
-            }}>
-                <div style={{
+           }}>
+                <form style={{
                     display:"flex",
-                    flexDirection:"column", 
+                    flexDirection:"column",
                     alignItems:"center"
                 }}>
                     <label style={{
-                        color:"#FFFFFF",
-                        fontSize:"30px",
-                        fontFamily:"Bold",
-                    }}>
-                        Posts & Chats
-                    </label>
-                    <label style={{
-                        color:"#FFFFFF",
-                        fontSize:"25px",
                         fontFamily:"Regular",
+                        fontWeight:"bold",
+                        color: Colors.blueLight,
+                        fontSize:"25px",
                         fontStyle:"italic"
                     }}>
-                        Come talk with us
+                        User Name
                     </label>
-                </div>
-            </div>
+                    <input
+                        type={"text"} 
+                        style={{
+                            width: windowSize.width / 5
+                        }}
+                    />
+                </form>
+           </div>
         </div>
     );
 }
