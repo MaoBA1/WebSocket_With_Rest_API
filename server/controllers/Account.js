c25f0d30446091a62836bbc0bf1cdac12c261202
Maor Bar Asher <94516860+MaoBA1@users.noreply.github.com>
2023-02-02 22:08:43 +0200

    123

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -4,13 +4,12 @@ const Account = require('../models/Account');
 const { auth } = require('../firebase');
 const { 
     createUserWithEmailAndPassword,
-    updateProfile,
     sendEmailVerification
 } = require('firebase/auth');
 
 const accountEvents = (socket) => {
     
-    socket.on("create_account", async(data) => {
+    socket.on("create_account", (data) => {
         const {
             email,
             password,
@@ -18,14 +17,42 @@ const accountEvents = (socket) => {
             lname,
             profileImage
         } = data;
-        console.log(data);
-        await createUserWithEmailAndPassword(auth, email, password)
-        .then(result => {
-            console.log(result);
+        const lowercaseEmail = email.toLowerCase();
+        const formattedFname = fname[0].toUpperCase() + fname.substring(1, fname.length);
+        const formattedLname = lname[0].toUpperCase() + lname.substring(1, lname.length);
+
+        Account.findOne({ email: lowercaseEmail })
+        .then(account => {
+            if(account) {
+                socket.emit("create_account", {message: "user has already exist"});
+                return;
+            } 
+            const newAccount = new Account({
+                _id: mongoose.Types.ObjectId(),
+                email: lowercaseEmail,
+                fname: formattedFname,
+                lname: formattedLname,
+                profileImage: profileImage
+            })
+            return newAccount.save()
+            .then(async(new_account) => {
+                await createUserWithEmailAndPassword(auth, email, password)
+                .then(() => {
+                    socket.emit("create_account", {
+                        status: true,
+                        account: new_account
+                    });
+                })
+                .catch(error => {
+                    socket.emit("create_account", {
+                        status: false,
+                        message: error.message
+                    })
+                })
+            })
+            
         })
-        .catch(error => {
-            console.log(error);
-        })
+        
     })
 }
 
