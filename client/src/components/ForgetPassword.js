aa90603e0733dd8e7ee14519475c5b86d0c0dcea
Maor Bar Asher <94516860+MaoBA1@users.noreply.github.com>
2023-02-06 17:28:56 +0200

    123

--- client/src/components/ForgetPassword.js
+++ client/src/components/ForgetPassword.js
@@ -25,6 +25,12 @@ function ForgetPassword({ setVisible }) {
                 setErrorMessage(response.message);
                 return
             }
+            console.log(response);
+            setSuccess(true);
+            setTimeout(() => {
+                setSuccess(false);
+                setVisible();
+            })
 
         });
     },[]);
@@ -64,70 +70,80 @@ function ForgetPassword({ setVisible }) {
                     boxShadow:"#000000 0px 5px 15px",
                     zIndex:"2",
                 }}>
-                    <div style={{
-                        width:"100%",
-                        display:"flex",
-                        flexDirection:"column",
-                        alignItems:"flex-end"
-                    }}>
-                        <RiCloseFill
-                            color={Colors.blueMedium}
-                            size="30px"
-                            onClick={setVisible}
-                        />
-                    </div>
-                    <label style={{
-                        fontFamily:"Bold",
-                        color: Colors.blueLight,
-                        fontSize:"20px",
-                        textAlign:"center"
-                    }}>
-                        What is your email Address ?
-                    </label>
-                    {errorMessage !== "" && <label style={{
-                        fontFamily:"Regular",
-                        color: Colors.red,
-                        fontSize:"18px",
-                        textAlign:"center"
-                    }}>
-                        {errorMessage}
-                    </label>}
-
-                    {success && <label style={{
-                        fontFamily:"Bold",
-                        color: Colors.blueMedium,
-                        fontSize:"18px",
-                        textAlign:"center"
-                    }}>
-                        We sent email to reset your password
-                    </label>}
-                    <form onSubmit={sendEmailPasswordLink}>
-                        <input 
-                            required
-                            value={email}
-                            onChange={event => setEmail(event.target.value)}
-                            style={{ 
-                                fontFamily:"Regular",
-                                border: `2px solid ${Colors.blueLight}`,
-                                backgroundColor:Colors.grey,
-                            }}
-                            placeholder="Email Address..."
-                        />
-                    </form>
-                    <button onClick={sendEmailPasswordLink} style={{
-                        backgroundColor:Colors.blueBold,
-                        border:`3px solid ${Colors.blueLight}`,
-                        fontFamily:"Bold",
-                        fontSize:"12px",
-                        padding:"10px",
-                        width:"max-content",
-                        display:"flex",
-                        flexDirection:"column",
-                        alignItems:"center",
-                        justifyContent:"center"
-                    }}>
-                        Send reset password link
-                    </button>   
+                    {
+                        success ?
+                        (
+                            <label style={{
+                                fontFamily:"Bold",
+                                color: Colors.blueMedium,
+                                fontSize:"18px",
+                                textAlign:"center"
+                            }}>
+                                We sent email to reset your password
+                            </label>
+                        )
+                        :
+                        (
+                            <>
+                                <div style={{
+                                    width:"100%",
+                                    display:"flex",
+                                    flexDirection:"column",
+                                    alignItems:"flex-end"
+                                }}>
+                                    <RiCloseFill
+                                        color={Colors.blueMedium}
+                                        size="30px"
+                                        onClick={setVisible}
+                                    />
+                                </div>
+                                <label style={{
+                                    fontFamily:"Bold",
+                                    color: Colors.blueLight,
+                                    fontSize:"20px",
+                                    textAlign:"center"
+                                }}>
+                                    What is your email Address ?
+                                </label>
+                                {errorMessage !== "" && <label style={{
+                                    fontFamily:"Regular",
+                                    color: Colors.red,
+                                    fontSize:"18px",
+                                    textAlign:"center"
+                                }}>
+                                    {errorMessage}
+                                </label>}
+                                <form onSubmit={sendEmailPasswordLink}>
+                                    <input 
+                                        required
+                                        value={email}
+                                        onChange={event => setEmail(event.target.value)}
+                                        style={{ 
+                                            fontFamily:"Regular",
+                                            border: `2px solid ${Colors.blueLight}`,
+                                            backgroundColor:Colors.grey,
+                                        }}
+                                        placeholder="Email Address..."
+                                    />
+                                </form>
+                                <button onClick={sendEmailPasswordLink} style={{
+                                    backgroundColor:Colors.blueBold,
+                                    border:`3px solid ${Colors.blueLight}`,
+                                    fontFamily:"Bold",
+                                    fontSize:"12px",
+                                    padding:"10px",
+                                    width:"max-content",
+                                    display:"flex",
+                                    flexDirection:"column",
+                                    alignItems:"center",
+                                    justifyContent:"center"
+                                }}>
+                                    Send reset password link
+                                </button>   
+                            </>
+                        )
+                    }
+                    
                 </div>
             </div>
         </div>
