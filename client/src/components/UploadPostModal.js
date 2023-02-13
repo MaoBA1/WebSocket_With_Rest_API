c38cb8863daf7912c91032d5bc5d04ca57b293fd
Maor Bar Asher <94516860+MaoBA1@users.noreply.github.com>
2023-02-13 20:36:03 +0200

    123

--- client/src/components/UploadPostModal.js
+++ client/src/components/UploadPostModal.js
@@ -6,10 +6,35 @@ function UploadPostModal({ account, setIsVisible }) {
     const [ componentIndex, setComponentIndex ] = useState(0);
 
     const components = [
-        <div className={componentIndex % 2 === 0 ? "slide active" : "slide" } style={{
-            
-        }}>
-            
+        <div 
+            className={componentIndex % 2 === 0 ? "slide active" : "slide" } 
+            style={{
+                
+            }}
+        >
+            <label style={{
+                fontFamily:"italic",
+                fontSize:"20px",
+                color: Colors.blueLight,
+                margin:"10px",
+                textAlign:"center"
+            }}>
+                What do you want to share ?
+            </label>
+            <textarea
+                type={"text"}
+                style={{
+                    width:"90%",
+                    height:"50%",
+                    backgroundColor:Colors.grey,
+                    border:`2px solid ${Colors.blueLight}`,
+                    fontFamily:"italic",
+                    borderRadius:"20px",
+                    padding:"10px"                    
+                }}
+                multiple
+                placeholder={"This is the place to share..."}
+            />
         </div>,
         <div className={componentIndex % 2 === 1 ? "slide active" : "slide" } style={{
             
