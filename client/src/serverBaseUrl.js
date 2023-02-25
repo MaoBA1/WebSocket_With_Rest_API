3b7ec70e32c1538a0f36dbbffc71f83a1a130238
Maor Bar Asher <94516860+MaoBA1@users.noreply.github.com>
2023-02-25 13:30:40 +0200

    123

--- client/src/serverBaseUrl.js
+++ client/src/serverBaseUrl.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line
 export default {
     // url: "http://192.168.1.41:3002/api",
     url: "https://postandchats.onrender.com/api",
--- server/index.js
+++ server/index.js
@@ -30,6 +30,7 @@ const mongoUrl = `mongodb+srv://maor:wm2qpAw2cZ0nwpkJ@postsandchats.orle5k9.mong
 
 const server = http.createServer(app);
 const io = require('socket.io')(server, {
+    transports: ['websocket'],
     cors: {
         origin: "*",
         methods: [ "GET", "POST", "PUT", "DELETE" ]
