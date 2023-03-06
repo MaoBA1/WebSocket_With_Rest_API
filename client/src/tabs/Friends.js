7f4e804a8fe74307956dcc67e28462017f6dbd38
Maor Bar Asher <94516860+MaoBA1@users.noreply.github.com>
2023-03-06 19:50:35 +0200

    123

--- client/src/tabs/Friends.js
+++ client/src/tabs/Friends.js
@@ -31,17 +31,21 @@ function Friends({ account, socket }) {
     
     return ( 
         <Scrollbars className='friend-screen-container'>
-            <div style={{
-                padding:"10px"
-            }}>
-                <label style={{
-                    fontFamily:"italic",
-                    color: Colors.blueBold,
-                    fontSize:"18px"
+            {
+                allFriendsAcconts?.filter(f => f.status === "wait")?.length > 0 &&
+                <div style={{
+                    paddingLeft:"25px",
+                    padding:"10px"
                 }}>
-                    Friendship requests
-                </label>
-            </div>
+                    <label style={{
+                        fontFamily:"italic",
+                        color: Colors.blueBold,
+                        fontSize:"18px"
+                    }}>
+                        Friendship requests
+                    </label>
+                </div>
+            }
             <div 
                 className='friends-container'
                 style={{
@@ -50,6 +54,122 @@ function Friends({ account, socket }) {
             >
                 {
                     allFriendsAcconts?.filter(f => f.status === "wait")?.map((item, index) => 
+                    
+                        <div key={item?._id} className="friend-array-item">
+                            <img
+                                alt='profile'
+                                style={{ 
+                                    width: isBrowser ? "85px" : "60px",
+                                    height: isBrowser ? "85px" : "60px",
+                                    borderRadius:"50px",
+                                    border:`2px solid ${Colors.blueLight}`
+                                }}
+                                src={item?.profileImage}
+                            />
+                            <label style={{
+                                fontFamily:"italic",
+                                color: Colors.blueLight,
+                                marginTop:"10px",
+                                fontSize: isBrowser ? "18px" : "15px",
+                                textAlign:"center"
+                            }}>
+                                {item.fname + " " + item.lname}
+                            </label>
+                            <label style={{ 
+                                fontFamily:"italic",
+                                textAlign:"center",
+                                fontSize: isBrowser ? "15px" : "12px",
+                            }}>
+                                sent you friendship request
+                            </label>
+
+                            <div 
+                                className='confirm-or-ignore-container-relative'
+                                style={{ 
+                                    left: isBrowser && "60px",
+                                    top:"20px",
+                                    right: !isBrowser && "10px",
+                                }}
+                            >
+                                <div style={{
+                                    border:"1px solid #FFFFFF",
+                                    borderRadius:"20px",
+                                    padding:"10px",
+                                    backgroundColor:"green",
+                                    display:"flex",
+                                    flexDirection:"row",
+                                    alignItems:"center"
+                                }} onClick={() => socket?.emit("confirm_friendship", {acccountId: item._id })}>
+                                    <GiConfirmed
+                                        color='#FFFFFF'
+                                    />
+                                    {
+                                        labelVisbilty &&
+                                        <label style={{
+                                            fontFamily:"italic",
+                                            color:"#FFFFFF",
+                                            marginLeft:"5px"
+                                        }}>
+                                            Confirm
+                                        </label>
+                                    }
+                                </div>
+
+                                <div style={{
+                                    border:"1px solid #FFFFFF",
+                                    borderRadius:"20px",
+                                    padding:"10px",
+                                    backgroundColor:"red",
+                                    display:"flex",
+                                    flexDirection:"row",
+                                    alignItems:"center",
+                                    marginLeft:"5px"
+                                }} onClick={() => socket?.emit("cancel_friendship", {acccountId: item._id })}>
+                                    <AiOutlineClose
+                                        color='#FFFFFF'
+                                    />
+                                    {
+                                        labelVisbilty &&
+                                        <label style={{
+                                            fontFamily:"italic",
+                                            color:"#FFFFFF",
+                                            marginLeft:"5px"
+                                        }}>
+                                            Ignore
+                                        </label>
+                                    }
+                                </div>   
+                            </div>
+                        </div>
+                    )
+                }
+            </div>
+
+            <div style={{ marginTop:"20px" }} />
+
+            {
+                allFriendsAcconts?.filter(f => f.status === "requsted")?.length > 0 &&
+                <div style={{
+                    paddingLeft:"25px",
+                    padding:"10px"
+                }}>
+                    <label style={{
+                        fontFamily:"italic",
+                        color: Colors.blueBold,
+                        fontSize:"18px"
+                    }}>
+                        waiting for approve
+                    </label>
+                </div>
+            }
+            <div 
+                className='friends-container'
+                style={{
+                    gridTemplateColumns: isBrowser? "repeat(4, 370px)" : "repeat(2, 180px)",
+                }}
+            >
+                {
+                    allFriendsAcconts?.filter(f => f.status === "requsted")?.map((item, index) => 
                     
                         <div key={item?._id} className="friend-array-item">
                             <img
