cdb7d25e35318f924a3823e7275ab3acd57be08e
MaoBA1 <94516860+MaoBA1@users.noreply.github.com>
2023-03-28 22:54:55 +0300

    123

--- server/index.js
+++ server/index.js
@@ -5,12 +5,6 @@ const http = require('http');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
-const path = require('path');
-
-app.use(express.static(path.join(__dirname, './client/build')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/build'));
-});
 
 // Routers
 const userRouter = require('./routes/userRoutes');
