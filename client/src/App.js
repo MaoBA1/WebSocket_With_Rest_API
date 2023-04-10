178b03587ae23341d5d907d1af2f35650b6277ac
Maor Bar Asher <94516860+MaoBA1@users.noreply.github.com>
2023-04-10 19:52:26 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,7 @@ function App() {
   const setupSocket = () => {
     const token = localStorage.getItem("user_token");
     if (token && !socket) {
-      const newSocket = io(serverBaseUrl.localServerSocketUrl, {
+      const newSocket = io(serverBaseUrl.productionServerSocketUrl, {
         query: {
           token: token
         },
